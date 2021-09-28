37753925bb32f3c1e95f17bc42dedae033093e44
y0shimitsugh0st84 <68965220+y0shimitsugh0st84@users.noreply.github.com>
2021-09-28 22:49:30 +0200

    Update package.sh

--- src/package.sh
+++ src/package.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 ### every exit != 0 fails the script
 set -e
-set -u
-
 echo "Install Package - on  based ubuntu 20.04 LTS"
-
 DEBIAN_FRONTEND=noninteractive apt-get install -y \
-        apt-utils jq \
+        jq \
         nano \
         psmisc \
         sudo \
