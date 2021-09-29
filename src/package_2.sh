e73cc0ec4ab12a73f43f5c5c809e83c99f7c9b5b
y0shimitsugh0st84 <68965220+y0shimitsugh0st84@users.noreply.github.com>
2021-09-29 20:20:47 +0200

    Create package_2.sh

---
+++ src/package_2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+### every exit != 0 fails the script
+set -e
+echo "Install Package - on  based ubuntu 20.04 LTS"
+echo "Installing  dbus-x11 xauth xinit x11-xserver-utils xdg-utils"
+#apt-get install -y dbus-x11 xauth xinit x11-xserver-utils xdg-utils--no-cache=true
+DEBIAN_FRONTEND=noninteractive apt-get install -y --fix-missing  supervisor
