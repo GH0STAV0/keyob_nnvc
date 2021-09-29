753c87bd2ea4931e5a4aee1d460e80d75242536f
y0shimitsugh0st84 <68965220+y0shimitsugh0st84@users.noreply.github.com>
2021-09-29 21:36:17 +0200

    Update package_2.sh

--- src/package_2.sh
+++ src/package_2.sh
@@ -5,4 +5,4 @@ set -e
 echo "Install Package - on  based ubuntu 20.04 LTS"
 echo "Installing  dbus-x11 xauth xinit x11-xserver-utils xdg-utils"
 #apt-get install -y dbus-x11 xauth xinit x11-xserver-utils xdg-utils--no-cache=true
-DEBIAN_FRONTEND=noninteractive apt-get install -y --fix-missing  supervisor
+DEBIAN_FRONTEND=noninteractive apt-get install -y --fix-missing  supervisor pwgen
