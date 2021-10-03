6e5e9688fea37557875f4d27f35ff64a435ae301
y0shimitsugh0st84 <68965220+y0shimitsugh0st84@users.noreply.github.com>
2021-10-04 01:12:28 +0200

    Update startup.sh

--- src/xfce-startup/startup.sh
+++ src/xfce-startup/startup.sh
@@ -1,1 +1,27 @@
+
+#!/bin/bash
+export USER="root"
+export PASSWORD="123123123"
+echo "000000000000000000000000000000000000001111111111111111111111111111111111111"
+#touch /root/.Xauthority
+mkdir -p /root/.vnc/
+echo $PASSWORD | vncpasswd -f > /root/.vnc/passwd
+chmod 600 /root/.vnc/passwd
+
+
+vncserver
+vncserver -kill :1
+mv /root/.vnc/xstartup /root/.vnc/xstartup.bak
+cat <<EOF > /root/.vnc/xstartup
+#!/bin/bash
+xrdb /root/.Xresources
+startxfce4 &
+EOF
+chmod +x /root/.vnc/xstartup
+
+touch /root/.Xresources
+sudo vncserver :1 -depth 24 -geometry 1360x768 
+websockify -D --web=/usr/share/novnc/  6901 localhost:5901
+
+###########################################
 /usr/bin/supervisord -n
