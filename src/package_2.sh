821618790f62812314c4cffcfb8600ffcbab704d
y0shimitsugh0st84 <68965220+y0shimitsugh0st84@users.noreply.github.com>
2021-10-04 04:34:59 +0200

    Update package_2.sh

--- src/package_2.sh
+++ src/package_2.sh
@@ -11,4 +11,8 @@ dpkg -i xfwm4_4.14.5-1_amd64.deb
 apt update
 DEBIAN_FRONTEND=noninteractive apt-get install -y --fix-missing  supervisor pwgen openvpn autocutsel xfwm4=4.14.5-1
 
+echo "Install Package -firefox-53.0b9.tar.bz2"
 wget https://ftp.mozilla.org/pub/firefox/releases/53.0b9/linux-x86_64/en-US/firefox-53.0b9.tar.bz2 && tar -xf firefox-53.0b9.tar.bz2 -C /root/EXTRAT
+echo "Install bash color"
+echo "export PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '" >> ~/.bashrc
+
