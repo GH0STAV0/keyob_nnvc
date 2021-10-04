280f89c045b09cd57abb79528dd9e70357eb465c
y0shimitsugh0st84 <68965220+y0shimitsugh0st84@users.noreply.github.com>
2021-10-04 05:52:57 +0200

    Update package_2.sh

--- src/package_2.sh
+++ src/package_2.sh
@@ -2,6 +2,7 @@
 
 ### every exit != 0 fails the script
 set -e
+export NO_VNC_HOME=/usr/libexec/noVNCdim
 mkdir -p /root/EXTRAT/
 echo "Install Package - on  based ubuntu 20.04 LTS"
 echo "Installing  dbus-x11 xauth xinit x11-xserver-utils xdg-utils"
@@ -19,4 +20,11 @@ chmod +x /root/install_add/geckodriver && cp /root/install_add/geckodriver /usr/
 echo "Install Package PIP-3 ***** pymysql pyvirtualdisplay faker-e164 Faker PySocks stem  bs4 selenium  ConfigParser lxml  speechrecognition requests pyvirtualdisplay pydub"
 
 pip3 install pymysql pyvirtualdisplay faker-e164 Faker PySocks stem  bs4 selenium  ConfigParser lxml  speechrecognition requests pyvirtualdisplay pydub
+
+DEBIAN_FRONTEND=noninteractive apt-get install -y  \
+        python-numpy \
+&& mkdir -p "${NO_VNC_HOME}"/utils/websockify \
+&& wget -qO- https://github.com/novnc/noVNC/archive/v1.2.0.tar.gz | tar xz --strip 1 -C "${NO_VNC_HOME}" \
+&& wget -qO- https://github.com/novnc/websockify/archive/v0.9.0.tar.gz | tar xz --strip 1 -C "${NO_VNC_HOME}"/utils/websockify \
+&& chmod +x -v "${NO_VNC_HOME}"/utils/*.sh \
 
