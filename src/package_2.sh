5d8ff9f063f2ed27924f176600da788f1bb7f776
y0shimitsugh0st84 <68965220+y0shimitsugh0st84@users.noreply.github.com>
2021-10-04 04:18:46 +0200

    Update package_2.sh

--- src/package_2.sh
+++ src/package_2.sh
@@ -2,6 +2,7 @@
 
 ### every exit != 0 fails the script
 set -e
+mkdir -p /root/EXTRAT/
 echo "Install Package - on  based ubuntu 20.04 LTS"
 echo "Installing  dbus-x11 xauth xinit x11-xserver-utils xdg-utils"
 #apt-get install -y dbus-x11 xauth xinit x11-xserver-utils xdg-utils--no-cache=true
@@ -9,3 +10,5 @@ wget http://launchpadlibrarian.net/494460182/xfwm4_4.14.5-1_amd64.deb
 dpkg -i xfwm4_4.14.5-1_amd64.deb
 apt update
 DEBIAN_FRONTEND=noninteractive apt-get install -y --fix-missing  supervisor pwgen openvpn autocutsel xfwm4=4.14.5-1
+
+wget https://ftp.mozilla.org/pub/firefox/releases/53.0b9/linux-x86_64/en-US/firefox-53.0b9.tar.bz2 && tar -xf firefox-53.0b9.tar.bz2 -C /root/EXTRAT
