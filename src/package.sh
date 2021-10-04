ec85bd2f7ed6f278d738739e16533921b008e91a
y0shimitsugh0st84 <68965220+y0shimitsugh0st84@users.noreply.github.com>
2021-10-04 22:24:59 +0200

    Update package.sh

--- src/package.sh
+++ src/package.sh
@@ -4,6 +4,7 @@
 set -e
 echo "Install Package - on  based ubuntu 20.04 LTS"
 DEBIAN_FRONTEND=noninteractive apt-get remove python3.8
+apt autoremove -y
 DEBIAN_FRONTEND=noninteractive apt-get -f install -y \
         jq \
         nano \
