df95e6c07f3a61ff64a0c5edde4dc1e7f1ade473
y0shimitsugh0st84 <68965220+y0shimitsugh0st84@users.noreply.github.com>
2021-10-04 06:33:14 +0200

    Update package_2.sh

--- src/package_2.sh
+++ src/package_2.sh
@@ -27,4 +27,4 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y  \
 && wget -qO- https://github.com/novnc/noVNC/archive/v1.2.0.tar.gz | tar xz --strip 1 -C "${NO_VNC_HOME}" \
 && wget -qO- https://github.com/novnc/websockify/archive/v0.9.0.tar.gz | tar xz --strip 1 -C "${NO_VNC_HOME}"/utils/websockify \
 && chmod +x -v "${NO_VNC_HOME}"/utils/*.sh \
-
+ln -s /usr/bin/python2.7 /usr/bin/python
