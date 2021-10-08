fe1fe187ad40c44e1a725f2b5b6283459268039c
y0shimitsugh0st84 <68965220+y0shimitsugh0st84@users.noreply.github.com>
2021-10-08 04:00:57 +0200

    Update package_2.sh

--- src/package_2.sh
+++ src/package_2.sh
@@ -21,7 +21,7 @@ echo "export PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[0
 chmod +x /root/install_add/geckodriver && cp /root/install_add/geckodriver /usr/bin/geckodriver13
 echo "Install Package PIP-3 ***** pymysql pyvirtualdisplay faker-e164 Faker PySocks stem  bs4 selenium  ConfigParser lxml  speechrecognition requests pyvirtualdisplay pydub"
 pip3 install selenium==4.0.0.a7
-pip3 install pymysql pyvirtualdisplay faker-e164 Faker PySocks stem  bs4   ConfigParser lxml  speechrecognition requests pyvirtualdisplay pydub pyautogui
+pip3 install pymysql pyvirtualdisplay faker-e164 Faker PySocks stem  bs4   ConfigParser lxml  speechrecognition requests pyvirtualdisplay pydub pyautogui emojis emoji
 
 DEBIAN_FRONTEND=noninteractive apt-get install -y  \
         python-numpy \
