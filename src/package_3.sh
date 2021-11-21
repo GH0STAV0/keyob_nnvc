1efeecb6aae070800434b2c2d9b833ea98a459ec
GH0STAV0 <91119949+GH0STAV0@users.noreply.github.com>
2021-11-20 19:51:56 -0800

    Update package_3.sh

--- src/package_3.sh
+++ src/package_3.sh
@@ -2,7 +2,7 @@
 
 ### every exit != 0 fails the script
 
-export NO_VNC_HOME=/usr/libexec/noVNCdim
+export NO_VNC_HOME=/usr/share/novnc
 rm /headless/*.bz2*
 
 git clone https://github.com/GH0STAV0/ALL_HCL.git
