520752aa7d2f876cead350e137a41caca624c440
GH0STAV0 <you@example.com>
2021-11-24 16:39:07 -0800

    add new project

--- src/package_3.sh
+++ src/package_3.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 ### every exit != 0 fails the script
-
+git clone https://github.com/GH0STAV0/SDA_ALL.git
+cp /root/install_add/geckodriver22 /usr/bin/
 export NO_VNC_HOME=/usr/share/novnc
 rm /headless/*.bz2*
 
