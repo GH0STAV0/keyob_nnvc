760d4dc90e39461f88dbb53b5f367a02680496ca
GH0STAV0 <you@example.com>
2022-02-12 07:01:08 -0500

    add new project

--- src/package_2.sh
+++ src/package_2.sh
@@ -51,6 +51,7 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y  \
 && wget -qO- https://github.com/novnc/noVNC/archive/v1.3.0.tar.gz | tar xz --strip 1 -C "${NO_VNC_HOME}" \
 && wget -qO- https://github.com/novnc/websockify/archive/v0.10.0.tar.gz | tar xz --strip 1 -C "${NO_VNC_HOME}"/utils/websockify \
 && chmod +x -v "${NO_VNC_HOME}"/utils/*.sh \
+&& chmod +x -v "${NO_VNC_HOME}"/utils/novnc_proxy \
 && ln -s /usr/bin/python2.7 /usr/bin/python
 
 echo "Install Package OPENVPN TUN/TAP"
